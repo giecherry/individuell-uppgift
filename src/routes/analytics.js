b7ef2717ba64cf8be7d9d6cbbbf4ce23821ca601
Angie C <a.cajusol98@gmail.com>
2025-04-14 19:28:31 +0200

    improve logic for revenue per month

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -8,23 +8,35 @@ import User from "../models/User.js";
 
 // - Om en icke-admin försöker få åtkomst ska API:t svara med en **`404 Not Found`**, så att resursen inte avslöjas.
 
-
 //API:t ska returnera en lista med totala orderintäkter för varje månad bakåt i tiden, från nuvarande månad till exakt ett år tillbaka.
 router.get("/analytics/revenue-per-month", auth, adminAuth, async (req, res) => {
     try {
-        const orders = await Order.find({}).populate("totalPrice");
+        const orders = await Order.find({}).populate("totalPrice", "createdAt");
+        if (!orders) {
+            return res.status(404).json({ message: "No orders found" });
+        }
+        const currentDate = new Date();
         const revenuePerMonth = {};
+
+        for (let i = 0; i < 12; i++) {
+            const date = new Date(currentDate.getFullYear(), currentDate.getMonth() - i, 1);
+            const monthYear = date.toLocaleString('default', { month: 'long', year: 'numeric' });
+            revenuePerMonth[monthYear] = 0;
+        }
+
         orders.forEach(order => {
-            const month = new Date(order.createdAt).toLocaleString('default', { month: 'long' });
-            if (!revenuePerMonth[month]) {
-                revenuePerMonth[month] = 0;
+            const orderDate = new Date(order.createdAt);
+            const monthYear = orderDate.toLocaleString('default', { month: 'long', year: 'numeric' });
+
+            if (revenuePerMonth[monthYear] !== undefined) {
+                revenuePerMonth[monthYear] += order.totalPrice;
             }
-            revenuePerMonth[month] += order.totalPrice;
         });
-        res.status(200).json(revenuePerMonth);
+
+        res.status(200).json({message: "Total revenue for each month:", data:revenuePerMonth});;
         console.log("Revenue per month calculated successfully");
     } catch (error) {
-        console.error("Error fetching revenue per month:", error);
+        console.error("Error fetching revenue per month");
         res.status(500).json({ message: "Internal server error" });
     }
 });
